a163aa24878f0623211c60ed39c543f23970ac36
n00shoak <118372243+n00shoak@users.noreply.github.com>
2024-05-04 10:27:32 +0200

    0.01

--- Assets/Ressources/Projectile/simpleBullet/SY_SimpleBullet.cs
+++ Assets/Ressources/Projectile/simpleBullet/SY_SimpleBullet.cs
@@ -4,20 +4,26 @@ using UnityEngine;
 
 public class SY_SimpleBullet : MonoBehaviour
 {
+    
     private Rigidbody rb;
     private float maxLifettime, currentLifeTime;
-
-    private void Start()
+    
+    private void Awake()
     {
         maxLifettime = MN_GeneralManager.GetManagerfromGeneral<MN_Player>().playerStats.attackStats[5] + 1;
         rb = GetComponent<Rigidbody>();
         StartCoroutine(lifeTime());
-        rb.velocity += transform.forward * 35f;
+        Debug.Log("TEST" +  rb.velocity);
     }
+    
 
     public void ManualUpdate()
     {
-        if(currentLifeTime >= maxLifettime)
+
+        rb.velocity += transform.forward * 5f;
+
+
+        if (currentLifeTime >= maxLifettime)
         {
             StopAllCoroutines();
             Destroy(gameObject);
@@ -26,17 +32,18 @@ public class SY_SimpleBullet : MonoBehaviour
         // scale by time :
         //transform.localScale = Vector3.Lerp(Vector3.one, Vector3.zero, currentLifeTime / maxLifettime );
     }
-
+    
     IEnumerator lifeTime()
     {
         yield return new WaitForSeconds(0.1f);
         currentLifeTime += 1;
         StartCoroutine(lifeTime());
     }
-
+    
 
     private void OnCollisionEnter(Collision collision)
     {
         collision.gameObject.GetComponent<SY_Health>().TakeDamage();
     }
+    
 }
